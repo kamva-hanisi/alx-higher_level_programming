a6522b7d5bc52144dead02a1b567b30a95db385a
kamva-hanisi <lucashanisi@gmail.com>
2023-08-08 06:52:31 -0700

    loops,if ..,else

---
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -0,0 +1,28 @@
+#ifndef LISTS_H
+#define LISTS_H
+#include <stdlib.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ *
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+	listint_t;
+}
+
+def islower(c):
+def uppercase(str):
+def print_last_digit(number):
+def add(a, b):
+def pow(a, b):
+def fizzbuzz();
+listint_t *insert_node(listint_t **head, int number);
+
+#endif /* LISTS_H */
