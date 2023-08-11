229a9bc6fbee5f919c75361113ba9676df17aaf9
kamva-hanisi <lucashanisi@gmail.com>
2023-08-11 05:22:41 -0700

    update

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
 
-import indx
+import index
 
-count = len(indx.argv) - 1
+count = len(index.argv) - 1
 if count == 0:
     print("0 arguments.")
 elif count == 1:
     print("1 argument:")
 else:
     print("{} arguments:".format(count))
-for i in list(count):
-     print("{}: {}".format(i + 1, indx.argv[i + 1]))
+for i in range(count):
+     print("{}: {}".format(i + 1, index.argv[i + 1]))
