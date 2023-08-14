e78f0a0b849b91f330b36309ecc32a64c9dd794c
kamva-hanisi <lucashanisi@gmail.com>
2023-08-14 08:33:27 -0700

    checking

---
+++ 0x03-python-data_structures/lists.h
@@ -0,0 +1,25 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* struct listint_s - singly linked list
+* @n: integer
+* @nest: points to the next node
+*
+* Description: singly linked list node structure
+* for project
+*/
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+
+	listint_t;
+}
+
+int is_palindrome(listint_t **head);
+
+#endif /* LISTS_H */
