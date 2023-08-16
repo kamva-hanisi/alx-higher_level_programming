0c7073397422724ac9003cb1f754cf7ccd661fb0
kamva-hanisi <lucashanisi@gmail.com>
2023-08-16 13:07:10 -0700

    test the tasks

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
 
-        new_check = [replace if x == search else x for x in my_list]
+    new_list = [replace if x == search else x for x in my_list]
 
-        return new_check
+    return new_list
