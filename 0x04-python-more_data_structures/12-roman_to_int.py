8c8d64fc81adcdf62e5e037d1a29afccf207e756
kamva-hanisi <lucashanisi@gmail.com>
2023-08-16 14:32:17 -0700

    test perfect

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if type(roman_string) is not str or roman_string is None:
+    if type(roman_string) != str or roman_string is None:
         return 0
-        dict = {
+    dict =
+    {
         'I': 1,
         'V': 5,
         'X': 10,
@@ -11,7 +12,7 @@ def roman_to_int(roman_string):
         'D': 500,
         'M': 1000
     }
-        result = 0
+    result = 0
     for i in range(len(roman_string)):
         if i > 0 and dict[roman_string[i]] > dict[roman_string[i - 1]]:
             result += dict[roman_string[i]] - (dict[roman_string[i - 1]] * 2)
