803bfcd01c59bfced8ef940c5b8558e5200a5513
kamva-hanisi <lucashanisi@gmail.com>
2023-12-11 07:16:08 -0800

    looking the script

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const first = 'C is fun';
-const sec = 'Python is cool';
+const second = 'Python is cool';
 const third = 'JavaScript is amazing';
-console.log('first\nsec\nthird');
+console.log('{first}\n{second}\n{third}');
