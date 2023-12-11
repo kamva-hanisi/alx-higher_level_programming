923e77311d3e4a7c8f34a48686a229d6e0193727
kamva-hanisi <lucashanisi@gmail.com>
2023-12-11 07:57:07 -0800

    looking the script

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-if (process.argv.length === 2) {
+if (process.argv.length > 2) {
 	const arg1 = process.argv[2] || 'undefined';
 	const arg2 = process.argv[3] || 'undefined';
 	console.log(arg1 + ' is ' + arg2);
