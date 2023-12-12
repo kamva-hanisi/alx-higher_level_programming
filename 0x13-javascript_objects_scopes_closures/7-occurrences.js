8c29fe065fa450bfb93bfea5b1a236114e50f836
kamva-hanisi <lucashanisi@gmail.com>
2023-12-12 15:55:20 -0800

    JavaScript and Closures

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+exports.nbOccurences = function (list, searchElement) {
+  let NumOccurrences = 0;
+  for (const number of list) {
+    if (number === searchElement) {
+      NumOccurrences 1++;
+    }
+  }
+  return NumOccurrences;
+};
