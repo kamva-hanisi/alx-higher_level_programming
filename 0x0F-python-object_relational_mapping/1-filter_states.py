7dbca692c1e08352654f5ce3d2e1891a5ef256e5
kamva-hanisi <lucashanisi@gmail.com>
2023-12-17 07:44:41 -0800

    Results must be sorted with MySQL and SQULchemy

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,23 +1,20 @@
 #!/usr/bin/python3
 """Lists all the states with a name starting with N
-    from database hbtn_0e_0_usa
-    mysql username, mysql password and database name
+    from database hbtn_0e_0_usa.
 """
+
 
 import MySQLdb
 from sys import argv
 
-if __name__ == '__main__':
-    db = MySQLdb.connect(host='localhost',
-                         port=3306,
-                         user=argv[1],
-                         passwd=argv[2],
-                         db=argv[3])
 
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name BINARY 'N%'")
-    row = cursor.fetchall()
+if __name__ == '__main__':
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id")
+    row = cur.fetchall()
     for state in row:
         print(state)
-    cursor.close()
+    cur.close()
     db.close()
